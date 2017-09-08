a82a469f761633ea3be7588ee7e395096d9d769a
David Orbelian <david.orbelian@gmail.com>
2017-09-08 20:00:23 +0400

    Add MicroNetCore.AspNetCore.Proxy.Sample project.

---
+++ samples/MicroNetCore.AspNetCore.Proxy.Sample/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using MicroNetCore.AspNetCore.Proxy.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroNetCore.AspNetCore.Proxy.Sample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddProxy(Configuration);
+
+            return services.BuildServiceProvider();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseProxy();
+            app.UseMvc();
+        }
+    }
+}
